c8bf0c0b47d1a3938c37f732085283aa0fc6faa5
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2025-01-06 12:39:45 +0100

    working on sign in page

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,4 +1,11 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "NEXT-STORE";
+
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+
 export const LATEST_PRODUCTS_LIMIT = 4;
+
+export const signInDefaultValues = {
+  email: "",
+  password: "",
+};
